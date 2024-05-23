807721a133b4b9f04881189dcc542e7766ee5427
Lennart Koziollek <lennart.koziollek@stud.uni-due.de>
2024-05-24 00:54:14 +0200

    better defaults

--- config/config.go
+++ config/config.go
@@ -12,22 +12,25 @@ func SetupConfig() {
 	viper.SetConfigType("toml")
 	viper.AddConfigPath("$HOME/.config/themer")
 
-	viper.SetDefault("hyprland.enable", true)
+	viper.SetDefault("hyprland.enable", false)
 
-	viper.SetDefault("kitty.enable", true)
+	viper.SetDefault("kitty.enable", false)
 	viper.SetDefault("kitty.path", os.Getenv("HOME") + "/.config/kitty/theme.conf")
 
-	viper.SetDefault("nvim.enable", true)
+	viper.SetDefault("nvim.enable", false)
 	viper.SetDefault("nvim.path", os.Getenv("HOME") + "/.config/nvim/config.json")
 
-	viper.SetDefault("tmux.enable", true)
+	viper.SetDefault("tmux.enable", false)
 	viper.SetDefault("tmux.path", os.Getenv("HOME") + "/.config/tmux/themer.conf")
 
-	viper.SetDefault("zsh.enable", true)
+	viper.SetDefault("zsh.enable", false)
 	viper.SetDefault("zsh.path", os.Getenv("HOME") + "/.config/zsh/prompt.sh")
 
-  viper.SetDefault("walker.enable", true)
+  viper.SetDefault("walker.enable", false)
 	viper.SetDefault("walker.path", os.Getenv("HOME") + "/.config/walker/colors.sh")
+
+	viper.SetDefault("swww.enable", false)
+	viper.SetDefault("swww.wallpaper_dir", os.Getenv("HOME") + "/Documents/Wallpaper")
 
 	err := viper.ReadInConfig()
 	if err != nil {
